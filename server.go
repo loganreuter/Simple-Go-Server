b0ffae46f66d9dc703a4dc8af6f0b2e613bf4a2a
lreuter2020 <lreuter2020@gmail.com>
2021-07-05 22:32:23 -0700

    added graceful shutdown for best practice

--- server.go
+++ server.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
 )
+
+var port string = ":8080"
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
@@ -19,6 +26,31 @@ func main() {
 	app.HandleFunc("/", HomePage)
 	app.HandleFunc("/form", LoginPage)
 
-	log.Fatal(http.ListenAndServe(":8080", app))
-	fmt.Printf("Server listening at localhost:8080")
+	srv := &http.Server{
+		Addr:    port,
+		Handler: app,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started: http://localhost" + port)
+
+	<-done
+	log.Print("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Print("Server Exited Properly")
 }
